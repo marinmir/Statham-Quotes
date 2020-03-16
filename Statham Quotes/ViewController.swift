73f91f44fd8e148b32dd2f0022c604d99afab844
Miroshnichenko Marina <marinamirosh99@gmail.com>
2020-03-16 20:11:29 +0300

    Initial Commit

---
+++ Statham Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Statham Quotes
+//
+//  Created by Мирошниченко Марина on 16.03.2020.
+//  Copyright © 2020 Мирошниченко Марина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
